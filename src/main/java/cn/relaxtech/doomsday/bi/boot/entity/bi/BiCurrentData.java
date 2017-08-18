069b0d990112cae8ba66a8107aa73aef2fdd58d9
差不多先生 <freekkk@outlook.com>
2017-08-18 20:04:41 +0800

    2017-08-11外网发布 1.修改实时数据，生成曲线

--- src/main/java/cn/relaxtech/doomsday/bi/boot/entity/bi/BiCurrentData.java
+++ src/main/java/cn/relaxtech/doomsday/bi/boot/entity/bi/BiCurrentData.java
@@ -20,7 +20,8 @@ public class BiCurrentData {
     private Integer DAU;
     private Double  PAC;
     private Integer RUC;
-    @JsonFormat(pattern = "MM-dd HH:mm:ss",timezone="GMT+8")
+    @JsonFormat(pattern = "HH:mm:ss",timezone="GMT+8")
+    //@JsonFormat(pattern = "HH:mm:ss")
     private Timestamp createTime;
 
 }
